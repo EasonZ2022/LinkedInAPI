bbe0c887734c79bd3fdb569928044086a0b811c6
1343491089 <1343491089@qq.com>
2022-07-19 20:31:12 -0400

    Enable user to POST/DELETE profiles

--- saveprofile/views.py
+++ saveprofile/views.py
@@ -1,35 +1,37 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated 
-from .models import Profile, Skill
+from .models import Profile
 
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import UserSerializer, RegisterSerializer, ProfileSerializer, SkillSerializer
+from .serializers import UserSerializer, RegisterSerializer, ProfileSerializer
 from django.contrib.auth.models import User
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import generics
 from rest_framework import status
 
 # Class based view to Get User Details using Token Authentication
+# /get-details
 class UserDetailAPIView(APIView):
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
     def get(self,request,*args,**kwargs):
         user = User.objects.get(id=request.user.id)
-        print("id:")
+        print("/get-details id:")
         print(request.user.id)
         serializer = UserSerializer(user)
         return Response(serializer.data)
 
+# Delete user account using Token Authentication
+# /delete
 class DeleteUserAPIView(APIView):
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
     def delete(self,request,*args,**kwargs):
         id=request.user.id
-        print("id:")
+        print("/delete id:")
         print(id)
         if User.objects.filter(id=id).exists():
             user = User.objects.get(id=id)
@@ -46,13 +48,15 @@ class RegisterUserAPIView(generics.CreateAPIView):
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
     
+# /my-profiles
 class UserSavedProfileAPIView(APIView):
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
     def get(self,request,*args,**kwargs):
         id=request.user.id
-        print("id:")
+        print("/my-profiles id:")
         print(id)
+        print(request.data)
         if User.objects.filter(id=id).exists():
             profiles = User.objects.get(id=id).profile_set.all()
             print(profiles)
@@ -60,9 +64,33 @@ class UserSavedProfileAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(
-                {"response": "User Doesn't Exists"},
-                status=status.HTTP_400_BAD_REQUEST
-            )
+                {"response": "User Doesn't Exists"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def post(self,request,*args,**kwargs):
+        print("/my-profiles id:")
+        print(request.user.id)
+        data = request.data
+        data["owner"] = request.user.id      # Safety purpose, make sure the owner is current user
+        print(data)
+        serializer = ProfileSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,*args,**kwargs):
+        print("/my-profiles id:")
+        print(request.user.id)
+        ownerId = request.user.id
+        targetId = request.data.get('id')
+        if User.objects.filter(id=ownerId).exists():
+            profiles = User.objects.get(id=ownerId).profile_set
+            if profiles.filter(id=targetId).exists():
+                profile = profiles.get(id=targetId)
+                profile.delete()
+                return Response({"response":"Profile Deleted"}, status=status.HTTP_200_OK)
+        return Response(
+            {"response": "User or Profile Doesn't Exists"}, status=status.HTTP_400_BAD_REQUEST)        
 
 #The below are all for test purpose, to see all users, profiles, skills
 class UserView(viewsets.ModelViewSet):
@@ -87,6 +115,6 @@ class ProfileView(viewsets.ModelViewSet):
 #     serializer_class = ExperienceSerializer
 #     queryset = Experience.objects.all()    
     
-class SkillView(viewsets.ModelViewSet):
-    serializer_class = SkillSerializer
-    queryset = Skill.objects.all()        
+# class SkillView(viewsets.ModelViewSet):
+#     serializer_class = SkillSerializer
+#     queryset = Skill.objects.all()        
